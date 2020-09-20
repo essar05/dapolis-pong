0e330c046ee788f9fd0942c44bb0860b1c673396
Robert Drobota <robert.drobota@gmail.com>
2020-09-21 01:07:28 +0300

    WIP:Added support for box2d paddle colision with ball

--- source/models/Paddle.cpp
+++ source/models/Paddle.cpp
@@ -1,5 +1,6 @@
 #include "Paddle.h"
 #include <Ess3D/input/InputManager.h>
+#include <Ess3D/2d/utils/Utils2D.h>
 
 Paddle::Paddle() = default;
 Paddle::Paddle(const glm::vec2 &position, const glm::vec2 &size, GLuint textureId, const glm::vec4 &uv) :
@@ -9,8 +10,49 @@ Paddle::~Paddle() = default;
 
 bool Paddle::onUpdate(float deltaTime) {
   _position += _velocity * deltaTime;
+ 
 
   return true;
+}
+
+void Paddle::initializeBody(b2World* world) {
+    b2BodyDef bodyDef;
+    bodyDef.type = b2_dynamicBody;
+    bodyDef.position.Set(_position.x, _position.y);
+    bodyDef.angle = glm::radians(0.0f);
+
+    _body = world->CreateBody(&bodyDef);
+}
+
+void Paddle::initializeFixtures(b2World* world) {
+    b2Vec2 points[4] = {
+        b2Vec2(
+            _position.x - 2.0f,
+            _position.y - 100.0f
+        ),
+        b2Vec2(
+            _position.x + 2.0f,
+            _position.y - 100.0f
+        ),
+        b2Vec2(
+            _position.x + 2.0f,
+            _position.y + 100.0f
+        ),
+        b2Vec2(
+            _position.x - 2.0f,
+            _position.y + 100.0f
+        ),
+    };
+
+    b2PolygonShape shape;
+    shape.Set(points, 4);
+    
+    b2FixtureDef fixtureDef;
+    fixtureDef.shape = &shape;
+    fixtureDef.density = 1.0f;
+    fixtureDef.friction = 0.f; //0, as otherwise the ball will slowly lose velocity over time
+    fixtureDef.restitution = 1.f; // "bounciness"
+    _body->CreateFixture(&fixtureDef);
 }
 
 void Paddle::onInput(Ess3D::InputManager *inputManager) {
@@ -26,10 +68,6 @@ void Paddle::onInput(Ess3D::InputManager *inputManager) {
 
   this->setVelocity(direction * velocity);
 }
-
-void Paddle::initializeBody(b2World *world) {}
-
-void Paddle::initializeFixtures(b2World *world) {}
 
 void Paddle::setVelocity(const glm::vec2& velocity) {
 	_velocity = velocity;
